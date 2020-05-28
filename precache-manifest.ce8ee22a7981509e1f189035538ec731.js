d1d120a4931a8a1e8221d8d1c2df1de6f59a8738
Annestezia <cast.wheel@gmail.com>
2020-05-28 23:51:13 +1000

    Updates

---
+++ precache-manifest.ce8ee22a7981509e1f189035538ec731.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b1d674e59b70ca7e72a1a427f442ab9",
+    "url": "/fcc-drum-machine/index.html"
+  },
+  {
+    "revision": "b56b38e6103ec06a2e00",
+    "url": "/fcc-drum-machine/static/css/main.9cdbb460.chunk.css"
+  },
+  {
+    "revision": "0d009856a25b7dc24f03",
+    "url": "/fcc-drum-machine/static/js/2.55a3dc6e.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/fcc-drum-machine/static/js/2.55a3dc6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b56b38e6103ec06a2e00",
+    "url": "/fcc-drum-machine/static/js/main.042ea47e.chunk.js"
+  },
+  {
+    "revision": "e333e0bc7fa01190699d",
+    "url": "/fcc-drum-machine/static/js/runtime-main.cd8a1630.js"
+  },
+  {
+    "revision": "e88742a60e8f63b59e214ed822ea72cf",
+    "url": "/fcc-drum-machine/static/media/main-bg.e88742a6.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/fcc-drum-machine/precache-manifest.97a0e50d184bff010e1a7ae9e1391d22.js"
+  "/fcc-drum-machine/precache-manifest.ce8ee22a7981509e1f189035538ec731.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fcc-drum-machine/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
