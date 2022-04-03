8f2e37374f42e3334526d7b6089c74a0c3a58755
Annestezia <cast.wheel@gmail.com>
2022-04-03 22:04:12 +1000

    Updates

---
+++ precache-manifest.81e4a596a081fb21873b9ce55f749d0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1d28f2112ec914514f4",
+    "url": "/fcc-drum-machine/static/css/main.c940681d.chunk.css"
+  },
+  {
+    "revision": "f1d28f2112ec914514f4",
+    "url": "/fcc-drum-machine/static/js/main.f1d28f21.chunk.js"
+  },
+  {
+    "revision": "0ca1e12421b4370524e1",
+    "url": "/fcc-drum-machine/static/js/1.0ca1e124.chunk.js"
+  },
+  {
+    "revision": "6b38bc727055cad7116d",
+    "url": "/fcc-drum-machine/static/js/runtime~main.6b38bc72.js"
+  },
+  {
+    "revision": "e88742a60e8f63b59e214ed822ea72cf",
+    "url": "/fcc-drum-machine/static/media/main-bg.e88742a6.jpg"
+  },
+  {
+    "revision": "a455dfd90bbd32299a4727780eb36208",
+    "url": "/fcc-drum-machine/index.html"
+  }
+];
